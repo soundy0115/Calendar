fa5b79e66eea6038b2ef38ddb6153f25bc94c00b
soundy0115 <soundy0115@gmail.com>
2022-11-23 22:33:15 -0800

    layout prac

--- src/calendar/SimpleCalendarTester.java
+++ src/calendar/SimpleCalendarTester.java
@@ -1,10 +1,41 @@
 package calendar;
 
+import java.awt.Insets;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 public class SimpleCalendarTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame();
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		frame.setTitle("Simple Calendar");
+	
+		
+		JPanel panel = new JPanel();
+		
+		BoxLayout boxLO = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		panel.setLayout(boxLO);
+		panel.setBorder(new EmptyBorder(new Insets(50, 100, 50, 100)));
+		JButton b1 = new JButton("Button 1");
+		JButton b2 = new JButton("Button 2");
+		JButton b3 = new JButton("Button 3");
+		JButton b4 = new JButton("Button 4");
+		
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		panel.add(b4);
+		
+		frame.add(panel);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
