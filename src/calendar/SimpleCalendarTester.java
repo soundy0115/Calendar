1b73cc4dae9d22f12d8807e86d972c2cd786dbd0
soundy0115 <soundy0115@gmail.com>
2022-11-24 01:05:22 -0800

    Month view grid

--- src/calendar/SimpleCalendarTester.java
+++ src/calendar/SimpleCalendarTester.java
@@ -1,37 +1,51 @@
 package calendar;
 
-import java.awt.Insets;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class SimpleCalendarTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
-		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame.setTitle("Simple Calendar");
 	
 		
 		JPanel panel = new JPanel();
 		
-		BoxLayout boxLO = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		
+		
+		BoxLayout boxLO = new BoxLayout(panel, BoxLayout.X_AXIS);
 		panel.setLayout(boxLO);
-		panel.setBorder(new EmptyBorder(new Insets(50, 100, 50, 100)));
-		JButton b1 = new JButton("Button 1");
-		JButton b2 = new JButton("Button 2");
-		JButton b3 = new JButton("Button 3");
-		JButton b4 = new JButton("Button 4");
 		
-		panel.add(b1);
-		panel.add(b2);
-		panel.add(b3);
-		panel.add(b4);
+		ArrayList<JLabel> days = new ArrayList<JLabel>();
+		days.add(new JLabel("S", JLabel.CENTER));
+		days.add(new JLabel("M", JLabel.CENTER));
+		days.add(new JLabel("T", JLabel.CENTER));
+		days.add(new JLabel("W", JLabel.CENTER));
+		days.add(new JLabel("T", JLabel.CENTER));
+		days.add(new JLabel("F", JLabel.CENTER));
+		days.add(new JLabel("S", JLabel.CENTER));
 		
+		
+		JPanel monthPanel = new JPanel();
+		monthPanel.setLayout(new GridLayout(6, 7));
+		for(JLabel label : days) {
+			monthPanel.add(label);
+		}
+		for(int i = 1; i <= 35; i++) {
+			//JButton emptyButton = new JButton();
+			
+			String dayNumber = Integer.toString(i);
+			monthPanel.add(new JButton(dayNumber));
+		}
+		panel.add(monthPanel);
 		frame.add(panel);
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
